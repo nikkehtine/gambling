a4d40a3cf445ecbbfbfc9e12d6d29bfa390b2856
nikkehtine <nikkehtine@int.pl>
2023-11-16 22:35:21 +0100

    Add amount of lines function

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 // use rand::Rng;
 use std::io;
 
+const MAX_LINES: u32 = 3;
+
 fn main() {
-    deposit();
+    let balance: u32 = deposit();
+    let lines = get_number_of_lines();
 }
 
 fn deposit() -> u32 {
@@ -26,6 +29,30 @@ fn deposit() -> u32 {
             return amount;
         } else {
             println!("Amount must be greater than 0")
+        }
+    }
+}
+
+fn get_number_of_lines() -> u32 {
+    loop {
+        let mut lines = String::new();
+
+        println!("How many lines do you want to bet on? (1-{MAX_LINES})");
+        io::stdin()
+            .read_line(&mut lines)
+            .expect("Could not read line");
+
+        let lines: u32 = match lines.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please enter a valid number");
+                continue;
+            }
+        };
+
+        match lines {
+            1..=MAX_LINES => return lines,
+            _ => println!("Amount not in range"),
         }
     }
 }
