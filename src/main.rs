5a66e7cbcc5710e440e0f6b5fff62cc14ce2c460
nikkehtine <nikkehtine@int.pl>
2023-11-16 23:11:12 +0100

    Add a get bet amount function

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,29 @@
 use std::io;
 
 const MAX_LINES: u32 = 3;
+const MAX_BET: u32 = 100;
+const MIN_BET: u32 = 2;
 
 fn main() {
     let balance: u32 = deposit();
-    let lines = get_number_of_lines();
+
+    let mut lines: u32;
+    let mut bet: u32;
+    let mut total_bet: u32;
+
+    loop {
+        lines = get_number_of_lines();
+        bet = get_bet();
+        total_bet = bet * lines;
+
+        if total_bet > balance {
+            println!("You can't afford this bet. Your current balance is ${balance}");
+        } else {
+            break;
+        }
+    }
+
+    println!("You are betting ${bet} on {lines} lines. Total bet is ${total_bet}.");
 }
 
 fn deposit() -> u32 {
@@ -53,6 +72,33 @@ fn get_number_of_lines() -> u32 {
         match lines {
             1..=MAX_LINES => return lines,
             _ => println!("Amount not in range"),
+        }
+    }
+}
+
+fn get_bet() -> u32 {
+    loop {
+        let mut amount = String::new();
+
+        println!(
+            "How much $ would you like to bet? (${} - ${})",
+            MIN_BET, MAX_BET
+        );
+        io::stdin()
+            .read_line(&mut amount)
+            .expect("Could not read line");
+
+        let amount: u32 = match amount.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please enter a valid number");
+                continue;
+            }
+        };
+
+        match amount {
+            MIN_BET..=MAX_BET => return amount,
+            _ => println!("Amount should be between ${MIN_BET} and ${MAX_BET}"),
         }
     }
 }
