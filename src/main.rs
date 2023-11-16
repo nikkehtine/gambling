71882830c78db297a05c573963cc26a20cde7ca7
nikkehtine <nikkehtine@int.pl>
2023-11-16 22:15:58 +0100

    Add deposit function

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,31 @@
-use rand::Rng;
+// use rand::Rng;
 use std::io;
 
 fn main() {
-    println!("Hello, world!");
+    deposit();
+}
+
+fn deposit() -> u32 {
+    loop {
+        let mut amount = String::new();
+
+        println!("How much $ would you like to deposit?");
+        io::stdin()
+            .read_line(&mut amount)
+            .expect("Could not read line");
+
+        let amount: u32 = match amount.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please enter a valid number");
+                continue;
+            }
+        };
+
+        if amount > 0 {
+            return amount;
+        } else {
+            println!("Amount must be greater than 0")
+        }
+    }
 }
